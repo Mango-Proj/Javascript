82b468f0702eaed2afe9c1eb0043e06d673554cb
Mango-Proj <lukev3889@gmail.com>
2024-12-26 15:28:03 +0900

    Add React State-lifecycle

---
+++ Book1/React/State-lifecycle2/State-lifecycle2.js
@@ -0,0 +1,18 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
